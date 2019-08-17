8e00322cd21ecc969088617808821bc7ef119207
Niklas Lindorfer <niklas@lindorfer.com>
2019-08-17 13:03:07 +0200

    Added first models * Food * FoodLog * Meal * MealLog

--- tracker_app/models.py
+++ tracker_app/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Food(models.Model):
+    '''  A food item
+    Can have calory and macro nutrient information.
+    Information is per gram.
+    '''
+    name = models.CharField(null=False, max_length=50)
+    calories = models.IntegerField(null=True)
+    fat = models.IntegerField(null=True)
+    carbs = models.IntegerField(null=True)
+    protein = models.IntegerField(null=True)
+
+
+class FoodLog(models.Model):
+    ''' A log of a food item '''
+    food = models.ForeignKey(Food, null=False, on_delete=models.PROTECT)
+    amount = models.FloatField(null=False)
+    day = models.DateField(default=datetime.now)
+    timestamp = models.DateTimeField(default=datetime.now)
+    user = models.ForeignKey(User, related_name="food_logs", on_delete=models.CASCADE)
+
+
+class Meal(models.Model):
+    ''' A meal consisting of many food items 
+    Can have a unit description outlining the meal size (e.g. "one plate")
+    '''
+    name = models.CharField(null=False, max_length=50)
+    unit_description = models.CharField(null=True, max_length=50)
+    ingredients = models.ManyToManyField(FoodLog, related_name='meals')
+
+
+class MealLog(models.Model):
+    ''' A log of a meal '''
+    meal = models.ForeignKey(Meal, null=False, on_delete=models.PROTECT)
+    amount = models.FloatField(null=False)
+    day = models.DateField(default=datetime.now)
+    timestamp = models.DateTimeField(default=datetime.now)
+    user = models.ForeignKey(User, related_name='meal_logs', on_delete=models.CASCADE)
