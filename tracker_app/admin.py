b4ee8e4edc6305f6424231b65d2baf34a84deb51
Niklas <niklas@lindorfer.com>
2019-09-12 14:19:38 +0200

    Added admin page

--- tracker_app/admin.py
+++ tracker_app/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from tracker_app.models import Food, FoodLog, FoodAmount, Meal, MealLog
 
-# Register your models here.
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'calories', 'fat', 'carbs', 'protein', )
+
+
+class FoodLogAdmin(admin.ModelAdmin):
+    list_display = ('food', 'amount', 'day', 'timestamp', 'user', )
+
+
+class FoodAmountAdmin(admin.ModelAdmin):
+    list_display = ('food', 'amount', )
+
+
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('name', 'unit_description', )
+
+
+class MealLogAdmin(admin.ModelAdmin):
+    list_display = ('meal', 'amount', 'day', 'timestamp', 'user', )
+
+
+admin.site.register(Food, FoodAdmin)
+admin.site.register(FoodLog, FoodLogAdmin)
+admin.site.register(FoodAmount, FoodAmountAdmin)
+admin.site.register(Meal, MealAdmin)
+admin.site.register(MealLog, MealLogAdmin)
