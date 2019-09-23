fabae31358ec2e9c72126a9e1c95cdf274dfea7a
Niklas <niklas@lindorfer.com>
2019-09-23 17:27:58 +0100

    Added meal logs to profile page * Added template tags for computing aggregates

---
+++ tracker_app/templatetags/log_utils.py
@@ -0,0 +1,44 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag()
+def multiply(a, b, *args, **kwargs):
+    """ Simple multiplication function """
+    return a * b
+
+
+@register.simple_tag()
+def food_product(food_amount, key, *args, **kwargs):
+    """ Template tag for getting true calory (etc.) values of FoodAmounts.
+
+        Takes a FoodAmount object and an attribute name (key)
+        Returns the attribute value per gram multiplied by the amount
+    """
+    return food_amount.amount * getattr(food_amount.food, key)
+
+
+@register.simple_tag()
+def meal_product(meal_log, key, *args, **kwargs):
+    """ Template tag function for getting true calory (etc.) values of MealLogs.
+
+        Takes a MealLog object and attribute name (key)
+        Returns the food_product multiplied by the meal size
+    """
+    products = [food_product(amount, key)
+                for amount in meal_log.meal.ingredients.all()]
+    return sum(products) * meal_log.amount
+
+
+@register.simple_tag()
+def total_product(food_logs, meal_logs, key, *args, **kwargs):
+    """ Template tag for getting total sum of calory (etc.) values 
+        of all logged foods and meals.
+
+        Takes lists of FoodLog and MealLog objects and an attribute name (key)
+        Returns the total sum over the key attribute
+    """
+    total = sum(meal_product(m, key) for m in meal_logs) + \
+        sum(food_product(f.food_amount, key) for f in food_logs)
+    return total
--- tracker_app/views.py
+++ tracker_app/views.py
@@ -26,26 +26,14 @@ def profile(request, date):
     day = Day.objects.get_or_create(user=request.user, date=parsed_date)[0]
 
     food_logs = FoodLog.objects.all().filter(day=day)
-    # meal_logs_objects = MealLog.objects.all().filter(day=day)
-
-    daily_sum = {
-        'calories': 0,
-        'fat': 0,
-        'carbs': 0,
-        'protein': 0,
-    }
-    for log in food_logs:
-        daily_sum['calories'] += log.amount * log.food.calories
-        daily_sum['fat'] += log.amount * log.food.fat
-        daily_sum['carbs'] += log.amount * log.food.carbs
-        daily_sum['protein'] += log.amount * log.food.protein
+    meal_logs = MealLog.objects.all().filter(day=day)
 
     context = {
         'today': parsed_date,
         'yesterday': parsed_date - timedelta(days=1),
         'tomorrow': parsed_date + timedelta(days=1),
         'food_logs': food_logs,
-        'daily_sum': daily_sum,
+        'meal_logs': meal_logs,
     }
     return render(request, 'tracker_app/profile.html', context=context)
 
